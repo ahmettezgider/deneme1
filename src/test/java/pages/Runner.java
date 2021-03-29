fb6db00a9e7a776b41d296b13911df29d5803bc8
atezgider <atezgider@gmail.com>
2021-03-29 20:29:42 +0200

    3

--- src/test/java/pages/Runner.java
+++ src/test/java/pages/Runner.java
@@ -1,15 +1,22 @@
 package pages;
 
 
+import com.cucumber.listener.Reporter;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(
         tags = "@test1",
         features = {"src/test/java/pages/"},
         glue = {"pages"},
-        plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "summary" }
+        plugin = { //basit rapor oluşturan plugin
+                "html:target/cucumber-report",
+                "json:target/cucumber.json"
+        }
 )
 
 public class Runner extends AbstractTestNGCucumberTests {
+
+
 }
